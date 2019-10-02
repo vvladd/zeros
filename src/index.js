53aed182d8d5b8eac173ea9a2e8ebe849f63c48f
Vladislav <>
2019-10-02 11:16:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function zeros(expression) {
-  // your solution
-}
+  expression = expression.split("*");
+  let countTwo = 0;
+  let countFive = 0;
+
+  expression.forEach(element => {
+    let step = element.includes("!!") ? 2 : 1;
+    let numElement = parseInt(element);
+
+    while (numElement > 0) {
+      
+      let thisNum = numElement;
+      while (thisNum % 5 === 0) {
+        countFive++;
+        thisNum = thisNum / 5;
+      }
+
+      thisNum = numElement;
+      while (thisNum % 2 === 0) {
+        countTwo++;
+        thisNum = thisNum / 2;
+      }
+
+      numElement -= step;
+    }
+  });
+  return Math.min(countFive, countTwo);
+};
